be37891a5e49cbb34ad04fd267b54ead0bd09db8
mjh1583 <mjh1583@naver.com>
2021-03-04 11:43:23 +0900

    2일차 커밋

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -1,8 +1,12 @@
+
 package com.example.first;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
     // 멤버 변수 선언 -------------------------------------------------------
     private final static String TAG = "MainActivity";  // Log 출력 시 구분자
-
+    private LinearLayout joinLAY;
+    private LinearLayout change_bgcolor;
+    private TextView editText;
     // ---------------------------------------------------------------------
     // 멤버 메서드
     // ---------------------------------------------------------------------
@@ -23,24 +29,42 @@ public class MainActivity extends AppCompatActivity {
         // 사용자에게 보여지는 화면 설정
         // setContentView(R.layout.activity_main);
         // setContentView(R.layout.layout_linear);
-        setContentView(R.layout.join);
+        //setContentView(R.layout.join);
+        setContentView(R.layout.change_bgcolor);
 
         // 디버깅을 위한 로그 출력
         Log.i(TAG, "onCreate()");
         // 간단한 메시지 창 띄우기, 반드시 show() 해야 화면에 보임!!!
         Toast.makeText(MainActivity.this, "My App Create~~~", Toast.LENGTH_LONG).show();
 
+        /*// UI 요소 객체 가져오기  -- join.xml
+        joinLAY = (LinearLayout)findViewById(R.id.joinLAY);
+        joinLAY.setBackgroundColor(Color.YELLOW);*/
     }
 
     // onClick 속성 지원 Method -------------------------------------------
     // UI 요소 중 onClick 속성 설정한 경우 실행되는 메서드
-    public void clickFunc(View v) {
+   /* public void clickFunc(View v) {
 
         if(v.getId() == R.id.cancelBTN)
             Toast.makeText(MainActivity.this, "C A N C E L", Toast.LENGTH_SHORT).show();
         else if(v.getId() == R.id.okBTN)
             Toast.makeText(MainActivity.this, "OK Click!!", Toast.LENGTH_SHORT).show();
 
-    }
+    }  // join.xml의 클릭 메서드*/
+
+    public void clickFunc(View v) {
+        change_bgcolor = findViewById(R.id.bgColor);
+        editText = findViewById(R.id.editText_changeBGC);
+        
+        if(v.getId() == R.id.red) {
+            editText.setText("레드");
+            change_bgcolor.setBackgroundColor(Color.RED);
+        }
+        else if(v.getId() == R.id.blue) {
+            editText.setText("블루");
+            change_bgcolor.setBackgroundColor(Color.BLUE);
+        }
+    } //change_bgcolor.xml의 버튼 클릭 메서드
 
 }
