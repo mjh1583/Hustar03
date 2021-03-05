0ab162423fe0f7014552254fef8f9b5f7c4b9b48
mjh1583 <mjh1583@naver.com>
2021-03-05 17:23:28 +0900

    3일차 커밋

--- app/src/main/java/com/example/exam_resource/ImageActivity.java
+++ app/src/main/java/com/example/exam_resource/ImageActivity.java
@@ -14,24 +14,39 @@ import java.util.Random;
 public class ImageActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private Button button;
+
+    private int imgResIds[] = {R.drawable.bird, R.drawable.cat, R.drawable.cat2, R.drawable.dog, R.drawable.elk};
+    private int idx = 0;
 
     private Resources resources;
-    private Random random = new Random();
+    //private Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 화면 설정
         setContentView(R.layout.image_layout);
 
+        //화면에 존재하는 UI 요소 객체 가져오기
         imageView = findViewById(R.id.catIMG);
-        button = findViewById(R.id.ch_img_btn);
+
+        // res 폴더의 xml 지원 접근 객체
         resources = this.getResources();
     }
 
     public void onClick(View v) {
         if(v.getId() == R.id.ch_img_btn) {
-            int i = random.nextInt(5);
+            // 클릭할 때마다 ImageView Image 변경
+            imageView.setImageDrawable(resources.getDrawable(imgResIds[idx]));
+            imageView.setImageResource(imgResIds[idx]);
+            //idx = (idx == imgResIds.length - 1) ? 0 : ++idx;
+
+            if(idx == (imgResIds.length-1))
+                idx = 0;
+            else
+                idx++;
+            /*int i = random.nextInt(5);
 
             switch (i) {
                 case 0:
@@ -48,8 +63,7 @@ public class ImageActivity extends AppCompatActivity {
                     break;
                 case 4:
                     imageView.setImageResource(R.drawable.elk);
-                    break;
-            }
+                    break;*/
         }
     }
 }
