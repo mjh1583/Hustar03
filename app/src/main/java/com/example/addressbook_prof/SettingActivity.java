3a5976b7337eb6e41bb6b9f464a73f642c8361cf
mjh1583 <mjh1583@naver.com>
2021-03-10 17:25:10 +0900

    Event Listener Study + Practice 이벤트 리스너 연습

---
+++ app/src/main/java/com/example/addressbook_prof/SettingActivity.java
@@ -0,0 +1,153 @@
+package com.example.addressbook_prof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class SettingActivity extends AppCompatActivity {
+    // Member Variable ----------------------------------------------------
+    private final boolean D = true;
+    private final String TAG = "SettingActivity";
+
+    private EditText idETXT;
+
+    private RadioGroup themeGroup;
+    private RadioButton whiteRBTN;
+    private RadioButton darkRBTN;
+    private RadioButton blueRBTN;
+    private RadioButton rBTN;
+
+    private CheckBox asCBTN;
+    private CheckBox wifiCBTN;
+
+    private String userId = "";
+    private String theme = "";
+    private String select1 = "";
+    private String select2 = "";
+
+    // Member Method - Override -------------------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        // 초기화
+        init();
+
+        if(D) Log.i(TAG, "onCreate()");
+    }
+
+    // Member Method - Custom ---------------------------------------------
+    private void init() {
+        idETXT = findViewById(R.id.idETXT);
+
+        themeGroup = findViewById(R.id.themeGruop);
+        whiteRBTN = findViewById(R.id.whiteRBTN);
+        darkRBTN = findViewById(R.id.darkRBTN);
+        blueRBTN = findViewById(R.id.blueRBTN);
+
+        asCBTN = findViewById(R.id.asCBTN);
+        wifiCBTN = findViewById(R.id.wifiCBTN);
+
+        // 입력 막기
+        //idETXT.setInputType(InputType.TYPE_NULL);
+        idETXT.setEnabled(false);
+
+        // 라디오 버튼 중 하나라도 체크되어 있다면
+        if(whiteRBTN.isChecked() || darkRBTN.isChecked() || blueRBTN.isChecked()) {
+            themeGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    if(checkedId != 0) {
+                        rBTN = findViewById(checkedId);
+
+                        if(rBTN.isChecked())
+                            theme = rBTN.getText().toString();
+                        else
+                            return;
+                    }
+                }
+            });
+        }
+
+//        whiteRBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(isChecked) {
+//                    theme = whiteRBTN.getText().toString();
+//                }
+//            }
+//        });
+//
+//        darkRBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(isChecked) {
+//                    theme = darkRBTN.getText().toString();
+//                }
+//            }
+//        });
+//
+//        blueRBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(isChecked) {
+//                    theme = blueRBTN.getText().toString();
+//                }
+//            }
+//        });
+
+        asCBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    select1 = asCBTN.getText().toString();
+                else
+                    select1 = "";
+            }
+        });
+
+        wifiCBTN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    select2 = buttonView.getText().toString();
+                else
+                    select2 = "";
+            }
+        });
+
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.modBTN:
+                // 타입 허용
+                idETXT.setEnabled(true);
+                break;
+            case R.id.saveBTN:
+                userId = idETXT.getText().toString();
+                Log.i(TAG, "Save : " + userId + " - " + theme + " - " + select1 + " - " + select2);
+                break;
+            case R.id.cancelBTN:
+                idETXT.setText("");
+                idETXT.setEnabled(false);
+                themeGroup.clearCheck();
+                asCBTN.setChecked(false);
+                wifiCBTN.setChecked(false);
+                break;
+            default:
+                break;
+        }
+    }
+}
